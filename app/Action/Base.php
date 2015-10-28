cce35ac91e5db575a238dcf23e9695eaf2c6c771
kclabrador <kclabrador@cpi.com.ph>
2015-10-28 09:07:35 +0800

    Update Base.php

--- app/Action/Base.php
+++ app/Action/Base.php
@@ -6,7 +6,7 @@ use Kanboard\Event\GenericEvent;
 use Pimple\Container;
 
 /**
- * Base class for automatic actions
+ * Base class for automatic actions   
  *
  * @package action
  * @author  Frederic Guillot
